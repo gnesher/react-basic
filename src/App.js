6a583b81c7566b0e8153e3a388b3747274d74d04
Guy Nesher <guy_nesher_tp@bmc.com>
2017-08-08 14:13:36 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import MainContent from './components/MainContent';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+      </div>
+    );
+  }
+}
+
+export default App;
